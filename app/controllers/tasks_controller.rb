bb07d2eeea95aebef10bcf9a2c2dbf6d4e16e313
hiro <hiromu0617@i.softbank.jp>
2020-06-25 23:12:32 +0900

    add csv and pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :file_not_blunk, only: :import
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page]).per(50)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
+    end
   end
 
   def show
@@ -35,7 +41,8 @@ class TasksController < ApplicationController
     end
 
     if @task.save
-      TaskMailer.creation_email(task).deliver_now
+      TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
     else
       render :new
@@ -47,13 +54,22 @@ class TasksController < ApplicationController
     render :new unless @task.valid?
   end
 
+  def import
+    current_user.tasks.import(params[:file]) 
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+
   private
 
     def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :image)
     end
 
     def set_task
       @task = current_user.tasks.find(params[:id])
+    end
+
+    def file_not_blunk
+      redirect_to tasks_url unless params[:file]
     end
 end
